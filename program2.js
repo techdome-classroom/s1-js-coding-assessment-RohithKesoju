5427a7e53de14e8459e8ee3dff3e7ed0d78445ea
RohithKesoju <rohithadams574@gmail.com>
2024-11-11 13:36:51 +0530

    initial commit

--- program2.js
+++ program2.js
@@ -1,7 +1,25 @@
 const decodeTheRing = function (s, p) {
+      // write your code here
+  const dp = Array(s.length + 1).fill(null).map(() => Array(p.length + 1).fill(false));
+  dp[0][0] = true;
 
-    // write your code here
+  for (let j = 1; j <= p.length; j++) {
+    if (p[j - 1] === '*') {
+      dp[0][j] = dp[0][j - 1];
+    }
+  }
 
-  };
-  
-  module.exports = decodeTheRing;
+  for (let i = 1; i <= s.length; i++) {
+    for (let j = 1; j <= p.length; j++) {
+      if (p[j - 1] === '*') {
+        dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+      } else if (p[j - 1] === '?' || p[j - 1] === s[i - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      }
+    }
+  }
+
+  return dp[s.length][p.length];
+};
+
+module.exports = decodeTheRing;
